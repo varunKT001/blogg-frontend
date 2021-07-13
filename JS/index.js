9443c4e078e500f9971c1ac93c4ddbe24acdbbd7
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2021-07-13 19:50:24 +0530

    Initial commit: Added Login and Register form with Error/Success Messages

---
+++ JS/index.js
@@ -0,0 +1,121 @@
+const url = 'http://localhost:5000'
+
+let submitRegisterButton = document.getElementById('submit-register')
+let notification = document.getElementById('notification')
+let notificationText = document.getElementById('notification-text')
+let notificationClose = document.querySelector('.close-button')
+
+function initializeLoader(){
+    document.querySelector('.loading-screen').style.display = 'flex'
+}
+
+function stopLoader(){
+    document.querySelector('.loading-screen').style.display = 'none'
+}
+
+function showPopup(message, success){
+    if(success){
+        notification.style.border = '2px solid green'
+        notification.style.background = 'rgba(0, 255, 0, 0.05)'
+    }
+    else{
+        notification.style.border = '2px solid red'
+        notification.style.background = 'rgba(255, 0, 0, 0.05)'
+    }
+
+    notificationText.innerText = message
+    notification.style.display = 'flex'
+}
+
+function closePopup(){
+    notificationText.innerText = ''
+    notification.style.display = 'none'
+}
+
+
+
+async function verifySessionStatus(token){
+    let response = await fetch(`${url}/auth/verifyToken`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + token
+        }
+        
+    })
+    response = await response.json()
+    if (response.message = 'verified'){
+        location.href = '/views/homepage/homepage.html'
+    }
+}
+
+async function login(user){
+    let response = await fetch(`${url}/auth/login`, {
+        method: 'POST',
+        body: user,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    response = await response.json()
+
+    if(response.message == 'internal server error'){
+        showPopup(response.message + '!, ' + 'Try again', false)
+    }
+    else if(response.message == 'user not found'){
+        showPopup(response.message, false)
+    }
+    else if(response.message == 'password incorrect'){
+        showPopup(response.message, false)
+    }
+    else if(response.message == 'user logged in successfully'){
+        localStorage.setItem('token', response.token)
+        initializeLoader()
+        location.href = '/views/homepage/homepage.html'
+    }
+}
+
+async function register(user){
+    let response = await fetch(`${url}/auth/register`, {
+        method: 'POST',
+        body: user,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    response = await response.json()
+
+    if(response.message == 'internal server error'){
+        showPopup(response.message + '!, ' + 'Try again')
+    }
+    else if(response.message == 'user already registered'){
+        showPopup(response.message)
+    }
+    else if(response.message == 'username already exist'){
+        showPopup(response.message)
+    }
+    else if(response.message == 'user successfully registered'){
+        localStorage.setItem('success-message', response.message + ', ' + 'Login to continue')
+        initializeLoader()
+        location.href = '/login.html'
+    }
+}
+
+
+submitRegisterButton.addEventListener('click', (event)=>{
+    let name = document.getElementById('name').value
+    let username = document.getElementById('username').value
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+    let user = {
+        name,
+        username,
+        email,
+        password
+    }
+    user = JSON.stringify(user)
+    register(user)
+})
+
+notificationClose.addEventListener('click', closePopup)
+
---
+++ JS/login.js
@@ -0,0 +1,95 @@
+const url = 'http://localhost:5000'
+
+let submitLoginButton = document.getElementById('submit-login')
+let notification = document.getElementById('notification')
+let notificationText = document.getElementById('notification-text')
+let notificationClose = document.querySelector('.close-button')
+
+function initializeLoader(){
+    document.querySelector('.loading-screen').style.display = 'flex'
+}
+
+function stopLoader(){
+    document.querySelector('.loading-screen').style.display = 'none'
+}
+
+function showPopup(message, success){
+    if(success){
+        notification.style.border = '2px solid green'
+        notification.style.background = 'rgba(0, 255, 0, 0.05)'
+    }
+    else{
+        notification.style.border = '2px solid red'
+        notification.style.background = 'rgba(255, 0, 0, 0.05)'
+    }
+
+    notification.style.display = 'flex'
+    notificationText.innerText = message
+}
+
+function closePopup(){
+    notification.style.display = 'none'
+    notificationText.innerText = ''
+}
+
+async function verifySessionStatus(token){
+    let response = await fetch(`${url}/auth/verifyToken`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + token
+        }
+        
+    })
+    response = await response.json()
+    if (response.message = 'verified'){
+        location.href = '/views/homepage/homepage.html'
+    }
+}
+
+async function login(user){
+    let response = await fetch(`${url}/auth/login`, {
+        method: 'POST',
+        body: user,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    response = await response.json()
+
+    if(response.message == 'internal server error'){
+        showPopup(response.message + '!, ' + 'Try again', false)
+    }
+    else if(response.message == 'user not found'){
+        showPopup(response.message, false)
+    }
+    else if(response.message == 'password incorrect'){
+        showPopup(response.message, false)
+    }
+    else if(response.message == 'user logged in successfully'){
+        localStorage.setItem('token', response.token)
+        initializeLoader()
+        location.href = '/views/homepage/homepage.html'
+    }
+}
+
+window.addEventListener('load', ()=>{
+    if(localStorage.getItem('success-message')){
+        showPopup(localStorage.getItem('success-message'), true)
+    }
+    localStorage.clear()
+})
+
+submitLoginButton.addEventListener('click', (event)=>{
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+    let user = {
+        email,
+        password
+    }
+    console.log(user)
+    user = JSON.stringify(user)
+    login(user)
+})
+
+notificationClose.addEventListener('click', closePopup)
